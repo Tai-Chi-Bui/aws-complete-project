2f70a6622415099bb48c6cc19bb89e6b4a8909bf
Tai Bui <75408677+Tai-Chi-Bui@users.noreply.github.com>
2023-12-08 21:40:31 +0700

    Enhancement: Refactor codebase (#2)
    * Edit
    
    * edit
    
    * edit

--- backend-flask/services/home_activities.py
+++ backend-flask/services/home_activities.py
@@ -13,7 +13,7 @@ class HomeActivities:
 
       results = [{
         'uuid': '68f126b0-1ceb-4a33-88be-d90fa7109eee',
-        'handle':  'Andrew Brown',
+        'handle':  'Tai Bui',
         'message': 'Cloud is very fun!',
         'created_at': (now - timedelta(days=2)).isoformat(),
         'expires_at': (now + timedelta(days=5)).isoformat(),
