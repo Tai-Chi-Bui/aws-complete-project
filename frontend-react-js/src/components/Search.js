01f9e9cdd393e253d14c7f7e990315b0eec20937
Tai Bui <tai.c.bui@gmail.com>
2023-12-08 16:44:03 +0700

    update

--- frontend-react-js/src/components/Search.js
+++ frontend-react-js/src/components/Search.js
@@ -2,8 +2,8 @@ import './Search.css';
 
 export default function ActivityFeed(props) {
   return (
-    <div className='search_field'>
-      <input type='text' placeholder='Search Cruddur' />
+    <div className="search_field">
+      <input type="text" placeholder="Search Tai's App" />
     </div>
   );
 }
--- frontend-react-js/src/pages/SignupPage.js
+++ frontend-react-js/src/pages/SignupPage.js
@@ -1,13 +1,12 @@
 import './SignupPage.css';
-import React from "react";
-import {ReactComponent as Logo} from '../components/svg/logo.svg';
-import { Link } from "react-router-dom";
+import React from 'react';
+import { ReactComponent as Logo } from '../components/svg/logo.svg';
+import { Link } from 'react-router-dom';
 
 // [TODO] Authenication
 import { Auth } from 'aws-amplify';
 
 export default function SignupPage() {
-
   // Username is Eamil
   const [name, setName] = React.useState('');
   const [email, setEmail] = React.useState('');
@@ -17,10 +16,10 @@ export default function SignupPage() {
 
   const onsubmit = async (event) => {
     event.preventDefault();
-    setErrors('')
-    console.log('username',username)
-    console.log('email',email)
-    console.log('name',name)
+    setErrors('');
+    console.log('username', username);
+    console.log('email', email);
+    console.log('name', name);
     try {
       const { user } = await Auth.signUp({
         username: email,
@@ -30,94 +29,82 @@ export default function SignupPage() {
           email: email,
           preferred_username: username,
         },
-        autoSignIn: { // optional - enables auto sign in after user is confirmed
+        autoSignIn: {
+          // optional - enables auto sign in after user is confirmed
           enabled: true,
-        }
+        },
       });
       console.log(user);
-      window.location.href = `/confirm?email=${email}`
+      window.location.href = `/confirm?email=${email}`;
     } catch (error) {
-        console.log(error);
-        setErrors(error.message)
+      console.log(error);
+      setErrors(error.message);
     }
-    return false
-  }
+    return false;
+  };
 
   const name_onchange = (event) => {
     setName(event.target.value);
-  }
+  };
   const email_onchange = (event) => {
     setEmail(event.target.value);
-  }
+  };
   const username_onchange = (event) => {
     setUsername(event.target.value);
-  }
+  };
   const password_onchange = (event) => {
     setPassword(event.target.value);
-  }
+  };
 
   let el_errors;
-  if (errors){
-    el_errors = <div className='errors'>{errors}</div>;
+  if (errors) {
+    el_errors = <div className="errors">{errors}</div>;
   }
 
   return (
-    <article className='signup-article'>
-      <div className='signup-info'>
-        <Logo className='logo' />
+    <article className="signup-article">
+      <div className="signup-info">
+        <Logo className="logo" />
       </div>
-      <div className='signup-wrapper'>
-        <form 
-          className='signup_form'
-          onSubmit={onsubmit}
-        >
-          <h2>Sign up to create a Cruddur account</h2>
-          <div className='fields'>
-            <div className='field text_field name'>
+      <div className="signup-wrapper">
+        <form className="signup_form" onSubmit={onsubmit}>
+          <h2>Sign up to create a Tai's App account</h2>
+          <div className="fields">
+            <div className="field text_field name">
               <label>Name</label>
-              <input
-                type="text"
-                value={name}
-                onChange={name_onchange} 
-              />
+              <input type="text" value={name} onChange={name_onchange} />
             </div>
 
-            <div className='field text_field email'>
+            <div className="field text_field email">
               <label>Email</label>
-              <input
-                type="text"
-                value={email}
-                onChange={email_onchange} 
-              />
+              <input type="text" value={email} onChange={email_onchange} />
             </div>
 
-            <div className='field text_field username'>
+            <div className="field text_field username">
               <label>Username</label>
               <input
                 type="text"
                 value={username}
-                onChange={username_onchange} 
+                onChange={username_onchange}
               />
             </div>
 
-            <div className='field text_field password'>
+            <div className="field text_field password">
               <label>Password</label>
               <input
                 type="password"
                 value={password}
-                onChange={password_onchange} 
+                onChange={password_onchange}
               />
             </div>
           </div>
           {el_errors}
-          <div className='submit'>
-            <button type='submit'>Sign Up</button>
+          <div className="submit">
+            <button type="submit">Sign Up</button>
           </div>
         </form>
         <div className="already-have-an-account">
-          <span>
-            Already have an account?
-          </span>
+          <span>Already have an account?</span>
           <Link to="/signin">Sign in!</Link>
         </div>
       </div>
